6567a143ca526abae0d865ca9b2c4cd6821213bd
rcarl91 <carl_rosengren@telia.com>
2015-07-01 16:08:01 +0200

    wordpress

--- index.php
+++ index.php
@@ -1,30 +1,17 @@
 <?php
 /**
- * The main template file.
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package vantage
- * @since vantage 1.0
- * @license GPL 2.0
+ * @package WordPress
  */
 
-get_header(); ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-<div id="primary" class="content-area">
-
-	<div id="content" class="site-content" role="main">
-
-		<?php get_template_part( 'loops/loop', siteorigin_setting('blog_archive_layout') ) ?>
-
-	</div><!-- #content .site-content -->
-
-</div><!-- #primary .content-area -->
-
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
