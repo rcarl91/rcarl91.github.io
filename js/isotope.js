5c74a217a49239f3e0881083757e1573a08d4bc0
rcarl91 <carl_rosengren@telia.com>
2015-07-18 19:41:55 +0200

    gg

--- js/isotope.js
+++ js/isotope.js
@@ -51,5 +51,17 @@ $( document ).ready( function() {
       $( this ).addClass('is-checked');
     });
   });
+
+$(".element-item").click(function(e){
+  e.preventDefault();
+  $(".popup").fadeIn(300,function(){$(this).focus();});
+});
+
+$('.close').click(function() {
+   $(".popup").fadeOut(300);
+});
+$(".popup").on('blur',function(){
+    $(this).fadeOut(300);
+});
  
 });
